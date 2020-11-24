a5449e541792711098571c2f6bdff1d031d417fe
Eder Díaz <ederchrono@gmail.com>
2020-11-23 18:06:17 -0600

    :sparkles: add mocked server

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+import axios from 'axios'
+import {makeServer} from './mockedServer.js'
 /* Add JavaScript code here! */
 console.log('Hello World! You did! Welcome to Snowpack :D');
+
+makeServer()
+
+axios.get('/api/users')
+  .then(response => response.data.users)
+  .then(console.table)
+
+
+axios.post('/api/users', {name: 'Eder'})
+  .then(response => response.data)
+  .then(console.log)
+
+axios.get('/api/users')
+  .then(response => response.data.users)
+  .then(console.table)
